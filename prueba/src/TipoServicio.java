ec95ce087ae8f0a759e53c6cab30d29a0c10b8ec
Diego Hernandez <pintosd.hernandez27@gmail.com>
2026-08-29 19:04:10 -0500

    Ey manin verifica ahi si quedo fino

---
+++ prueba/src/TipoServicio.java
@@ -0,0 +1,27 @@
+public enum TipoServicio {
+    CORTE_CABELLO("Corte de Cabello", 15000.0, 30),
+    AFEITADO("Afeitado", 10000.0, 20),
+    TRATAMIENTO_BARBA("Tratamiento de Barba", 20000.0, 40);
+
+    private final String nombre;
+    private final double precio;
+    private final int duracionMin;
+
+    TipoServicio(String nombre, double precio, int duracionMin) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.duracionMin = duracionMin;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getDuracionMin() {
+        return duracionMin;
+    }
+}
